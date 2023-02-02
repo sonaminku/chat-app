490137f2e2a273bafcc2bef4c1bb36ffb723d271
sonaminku <110347555+sonaminku@users.noreply.github.com>
2023-02-02 22:08:46 +0530

    Created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Loader } from 'rsuite'
+import { useParams } from 'react-router'
+import ChatTop from '../../components/chat-window/top'
+import Messages from '../../components/chat-window/messages'
+import ChatBottom from '../../components/chat-window/bottom'
+import {useRooms} from '../../context/rooms.context'
 
 const Chat = () => {
+    const {chatId} = useParams()
+    const rooms = useRooms()
+    if (!rooms){
+        return <Loader center vertical size="md" content="Loading" speed='slow' />
+    }
+    const currentRoom = rooms.find(room => room.id === chatId)
+    if(!currentRoom){
+        return <h6 className='text-center mt-page'>Chat {chatId} not found</h6>
+    }
   return (
-    <div>
-      hello
-    </div>
+    <>
+      <div className='chat-top'>
+        <ChatTop />
+      </div>
+      <div className='chat-middle'>
+        <Messages />
+      </div>
+      <div className='chat-bottom'>
+        <ChatBottom />
+      </div>
+    </>
   )
 }
 
