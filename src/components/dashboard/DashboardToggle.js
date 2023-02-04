5587b31052dda69207b8aa12856c7113cd1cfbb7
sonaminku <110347555+sonaminku@users.noreply.github.com>
2023-02-04 23:10:07 +0530

    add realtime presence listener for a user

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,27 +1,34 @@
-import React, { useCallback } from 'react'
-import { Alert, Button, Drawer, Icon } from 'rsuite'
-import DashBoard from '.'
-import { useMediaQuery, useModalState } from '../../misc/custom-hooks'
-import { auth } from '../../misc/firebase'
+import React, { useCallback } from 'react';
+import { Alert, Button, Drawer, Icon } from 'rsuite';
+import DashBoard from '.';
+import { isOfflineForDatabase } from '../../context/profile.context';
+import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
-    const {isOpen, open, close} = useModalState()
-    const isMobile = useMediaQuery('(max-width: 992px)');
-    const onSignOut = useCallback(()=>{
-        auth.signOut()
-        Alert.info('Signed out', 4000)
-        close()
-    },[close])
+  const { isOpen, open, close } = useModalState();
+  const isMobile = useMediaQuery('(max-width: 992px)');
+  
+  const onSignOut = useCallback(() => {
+    database.ref(`/status/${auth.currentUser.uid}`).set(isOfflineForDatabase).then(()=>{
+      auth.signOut();
+      Alert.info('Signed out', 4000);
+      close();
+    }).catch(err => {
+      Alert.error(err.message, 4000);
+    })
+  }, [close]);
   return (
     <div>
-      <Button block color='blue' onClick={open}>
-        <Icon icon="dashboard" />Dashboard
+      <Button block color="blue" onClick={open}>
+        <Icon icon="dashboard" />
+        Dashboard
       </Button>
-      <Drawer full={isMobile} show={isOpen} onHide={close} placement='left'>
+      <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <DashBoard onSignOut={onSignOut} />
       </Drawer>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardToggle
+export default DashboardToggle;
